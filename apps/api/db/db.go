3d09586e495e84bc89dccc8924b3090a2c96a1c1
Hiroki Kajiwara <rerun.0510@gmail.com>
2023-12-10 17:31:55 +0900

    add api

---
+++ apps/api/db/db.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDB() *gorm.DB {
+	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PW"), os.Getenv("POSTGRES_HOST"),
+		os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
+	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Connected 🚀")
+	return db
+}
+
+func CloseDB(db *gorm.DB) {
+	sqlDB, _ := db.DB()
+	if err := sqlDB.Close(); err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ apps/api/repository/user_repository.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"go-rest-api/model"
+
+	"gorm.io/gorm"
+)
+
+type IUserRepository interface {
+	GetUserByEmail(user *model.User, email string) error
+	CreateUser(user *model.User) error
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) IUserRepository {
+	return &userRepository{db}
+}
+
+// GetUserByEmail implements IUserRepository.
+func (ur *userRepository) GetUserByEmail(user *model.User, email string) error {
+	if err := ur.db.Where("email=?", email).First(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// CreateUser implements IUserRepository.
+func (ur *userRepository) CreateUser(user *model.User) error {
+	if err := ur.db.Create(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
