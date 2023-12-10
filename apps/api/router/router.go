c2c10d2a45376ce6624967d3fbd1c22cd643e343
Hiroki Kajiwara <rerun.0510@gmail.com>
2023-12-10 19:02:12 +0900

    fix: FE port

--- apps/api/router/router.go
+++ apps/api/router/router.go
@@ -15,7 +15,7 @@ func NewRouter(uc controller.IUserController, tc controller.ITaskController) *ec
 
 	// CORS
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")},
+		AllowOrigins: []string{"http://localhost:5173", os.Getenv("FE_URL")},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept,
 			echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken},
 		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
